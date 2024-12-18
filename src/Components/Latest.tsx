dae83ccb11f1db78e8cae725fe9e6eb7a372d540
SHAHBAZ-AHMED1122 <161025292+SHAHBAZ-AHMED1122@users.noreply.github.com>
2024-12-18 15:06:17 +0500

    small the letter "c" in word "component" to resolve deployment issue
    small the letter "c" in word "component" to resolve deployment issue

--- src/Components/Latest.tsx
+++ src/Components/Latest.tsx
@@ -24,16 +24,21 @@ function Latest() {
             <Image className='p-1' src="/Blog Card 3.png" alt='' width={400} height={400}/>
           </div>
           </div>
-          <div className='flex items-center justify-between  border-b border-yellow-500 pb-20'>
+          <div className='flex flex-col  md:flex md:flex-row md:items-center justify-between  border-b border-yellow-500 pb-20'>
 
           <div>
-          <h1 className="font-bold text-[48px] text-white pt-4 pb-4 ">
+          <h1 className="text-4xl sm:text-[48px] font-bold  text-white pt-4 pb-4 ">
             <span className="text-yellow-500">St</span>ill you need our support<br />
           </h1> 
           <p className='text-white'>Don’t wait make a smart & logical quote here. Its pretty easy.</p>
           </div>
-          <div className=''>
-            <input className='p-2 bg-yellow-400 text-white rounded-sm' type="text" placeholder='Enter your Email' /><span className='text-yellow-500 rounded-sm bg-white p-[11px] '>subscribe</span>
+          <div className='flex items-center'>
+<div>
+            <input className='p-2 bg-yellow-400 text-white rounded-sm' type="text" placeholder='Enter your Email' />
+</div>
+            <div>
+            <span className='text-yellow-500 rounded-sm bg-white pt-[11px] pb-[11px] '>subscribe</span>
+            </div>
           </div>
           </div>
     </div>
